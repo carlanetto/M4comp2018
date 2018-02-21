51a2f8182ebce00dd1f13c253a156fe5780e6b70
pmont <pmontm@gmail.com>
2018-02-21 20:39:54 +1100

    Added the R file with the explanation of the dataset Created the NAMESPACE file using roxygen2

---
+++ R/M4.R
@@ -0,0 +1,22 @@
+#' M4 Competiotion data
+#'
+#' Featuring 100000 of diffrerent domains
+.
+#'
+#' @docType data
+#'
+#' @usage data(M4)
+#'
+#' @format An list of  \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Moore et al. (2013) Genetics 195:1077-1086
+#' (\href{https://www.m4.unic.ac.cy/resources/makridakis-competitions/}{M4 Competition Web})
+#'
+#' @source \href{https://www.m4.unic.ac.cy/the-dataset/}{M4 Competition Dataset}
+#'
+#' @examples
+#' data(M4)
+#' names(M4[[1]])
+"M4"
